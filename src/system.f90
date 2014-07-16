2dcf9067272aab5567d476c9a5a1c45bb8829b8b
AstroFloyd <AstroFloyd@gmail.com>
2014-07-16 14:24:58 +0200

    Updated copyright

--- src/system.f90
+++ src/system.f90
@@ -1,7 +1,7 @@
 !> \file system.f90  System-related procedures
 
 
-!  Copyright (c) 2002-2013  AstroFloyd - astrofloyd.org
+!  Copyright (c) 2002-2014  AstroFloyd - astrofloyd.org
 !   
 !  This file is part of the libSUFR package, 
 !  see: http://libsufr.sourceforge.net/
